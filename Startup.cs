06bff3545eb1f143aa9fb1db94693dc9c57404be
Kontra <mk637215@ohio.edu>
2020-01-22 10:16:57 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KontraMIS4200.Startup))]
+namespace KontraMIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
