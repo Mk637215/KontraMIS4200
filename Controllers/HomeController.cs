44ef7ec5a28edc069fc0622c849dd45a34cd08ca
Matthew Kontra <mk637215@ohio.edu>
2020-01-27 10:01:50 -0500

    update for sd1 part 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace KontraMIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 Test Description.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "MY MIS4200 Test Description.";
 
             return View();
         }
