4e116f99a002ffb50f36903742b4a5f9803ccf53
Matthew Kontra <mk637215@ohio.edu>
2020-01-29 10:41:28 -0500

    IC 1/29

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using KontraMIS4200.Models;
+
+namespace KontraMIS4200.DAL
+{
+    public class MIS4200Context : DbContext 
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<customer> Customers { get; set; }
+        public DbSet<orders> Orders { get; set; }
+    }
+}
