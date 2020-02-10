fa80515666cd454fda2674fd0e648a09ff55eec4
Matthew Kontra <mk637215@ohio.edu>
2020-02-10 12:58:45 -0500

    Sd2

---
+++ Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KontraMIS4200.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Models/Vet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KontraMIS4200.Models
+{
+    public class Vet
+    {
+        public int vetId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
