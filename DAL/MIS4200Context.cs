f541794eb11d10841ad41b20196dc744e379d74c
Matthew Kontra <mk637215@ohio.edu>
2020-02-10 13:52:08 -0500

    sd2 1:52

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -17,6 +17,11 @@ namespace KontraMIS4200.DAL
         public DbSet<orders> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<orderDetail> orderDetail { get; set; }
+        public DbSet<Vet> Vet { get; set; }
+        public DbSet<Pet> Pet { get; set; }
 
+        public System.Data.Entity.DbSet<KontraMIS4200.Models.Vet> Vets { get; set; }
+
+        public System.Data.Entity.DbSet<KontraMIS4200.Models.Pet> Pets { get; set; }
     }
 }
---
+++ Models/Pet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KontraMIS4200.Models
+{
+    public class Pet
+    {
+        public int petId { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+
+    }
+}
